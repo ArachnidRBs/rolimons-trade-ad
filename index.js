e4781e5be2496aaee50ce118c4b40e3b119859c3
ArachnidRBs <89750511+ArachnidRBs@users.noreply.github.com>
2024-07-03 01:02:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ function generateAd() {
     let receivingSide = [];
     let totalSendValue = 0;
     for (const item of sendingSide) {
-      console.log(itemValues[item])
+      console.log(itemValues)
       totalSendValue = totalSendValue + itemValues[item].value;
     }
     //console.log("Total Send Value", totalSendValue);
